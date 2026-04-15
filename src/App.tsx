dd9904a71e389002175ec39cc76b031688f831a4
SamShah <sieam240@gmail.com>
2026-04-15 12:29:51 +0600

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import PatientList from './pages/PatientList';
@@ -17,7 +17,7 @@ import Reports from './pages/Reports';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
@@ -30,6 +30,6 @@ export default function App() {
           <Route path="reports" element={<Reports />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
